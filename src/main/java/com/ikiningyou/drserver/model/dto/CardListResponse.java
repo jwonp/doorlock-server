c50a0d2d81240d6381f892e4c8865689cccfa3e0
Joowon <tkdel222@gmail.com>
2023-06-12 18:31:49 +0900

    update get card list api

---
+++ src/main/java/com/ikiningyou/drserver/model/dto/CardListResponse.java
@@ -0,0 +1,39 @@
+package com.ikiningyou.drserver.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public interface CardListResponse {
+  public String getId();
+
+  public int getMaxSize();
+
+  public String getType();
+
+  // tech types flag
+
+  public boolean getIsIsoDep();
+
+  public boolean getIsNfcA();
+
+  public boolean getIsNfcB();
+
+  public boolean getIsNfcF();
+
+  public boolean getIsNfcV();
+
+  public boolean getIsNdef();
+
+  public boolean getIsNdefFormatable();
+
+  public boolean getIsMifareClassic();
+
+  public boolean getIsMifareUltralight();
+
+  public boolean getIsUsed();
+
+  public String getUserId();
+
+  public int getRoomId();
+}
--- src/main/java/com/ikiningyou/drserver/repository/CardRepository.java
+++ src/main/java/com/ikiningyou/drserver/repository/CardRepository.java
@@ -1,9 +1,23 @@
 package com.ikiningyou.drserver.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.ikiningyou.drserver.model.dao.Card;
+import com.ikiningyou.drserver.model.dto.CardListResponse;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CardRepository extends JpaRepository<Card,String> {
-    
+public interface CardRepository extends JpaRepository<Card, String> {
+  @Query(
+    value = "SELECT" +
+    " A.id, A.maxSize, A.type," +
+    " A.isIsoDep," +
+    " A.isNfcA, A.isNfcB, A.isNfcF, A.isNfcV," +
+    " A.isNdef, A.isNdefFormatable," +
+    " A.isMifareClassic, A.isMifareUltralight," +
+    " A.isUsed," +
+    " B.id, B.roomId" +
+    " FROM Card A LEFT JOIN User B ON A.id = B.cardId"
+  )
+  Optional<List<CardListResponse>> getCardListWithUserIdAndRoomId();
 }
