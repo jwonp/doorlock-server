936733b9e91ea630bb0534703b011c76787b636c
Joowon <tkdel222@gmail.com>
2023-06-21 19:51:05 +0900

    change dto path

---
+++ src/main/java/com/ikiningyou/drserver/model/dto/card/CardAddRequest.java
@@ -0,0 +1,15 @@
+package com.ikiningyou.drserver.model.dto.card;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class CardAddRequest {
+    private String id;
+    private int maxSize;
+    private String[] techTypes;
+    private String type;
+}
---
+++ src/main/java/com/ikiningyou/drserver/model/dto/card/CardListWithUserAndRoom.java
@@ -0,0 +1,37 @@
+package com.ikiningyou.drserver.model.dto.card;
+
+import java.util.Optional;
+
+public interface CardListWithUserAndRoom {
+  public String getId();
+
+  public int getMaxSize();
+
+  public String getType();
+
+  // tech types flag
+
+  public boolean getIsIsoDep();
+
+  public boolean getIsNfcA();
+
+  public boolean getIsNfcB();
+
+  public boolean getIsNfcF();
+
+  public boolean getIsNfcV();
+
+  public boolean getIsNdef();
+
+  public boolean getIsNdefFormatable();
+
+  public boolean getIsMifareClassic();
+
+  public boolean getIsMifareUltralight();
+
+  public boolean getIsUsed();
+
+  public String getUserId();
+
+  public Optional<Integer> getRoomId();
+}
