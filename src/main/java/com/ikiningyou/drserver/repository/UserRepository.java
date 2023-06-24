8b425f4124775d474149a5749b91d806c8a7e1d6
Joowon <tkdel222@gmail.com>
2023-06-24 22:56:32 +0900

    remove query

--- src/main/java/com/ikiningyou/drserver/repository/UserRepository.java
+++ src/main/java/com/ikiningyou/drserver/repository/UserRepository.java
@@ -1,20 +1,6 @@
 package com.ikiningyou.drserver.repository;
 
 import com.ikiningyou.drserver.model.dao.User;
-import com.ikiningyou.drserver.model.dto.user.UserListWithReservationResponse;
-
-import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-
-public interface UserRepository extends JpaRepository<User, String> {
-  @Query(
-    "SELECT " +
-    "A.id, A.name, A.phone, A.lastTagged, B.cardId, B.roomId " +
-    "FROM User A LEFT JOIN Reservation B ON A.id = B.userId"
-  )
-  Optional<List<UserListWithReservationResponse>> getUserListWithReservataion();
-
-}
+public interface UserRepository extends JpaRepository<User, String> {}
