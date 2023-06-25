272474d1aa7df257d3a5a80a5dfdd80e8f94a10e
Joowon <tkdel222@gmail.com>
2023-06-25 23:07:57 +0900

    add UserBuilder

--- src/main/java/com/ikiningyou/drserver/util/builder/user/UserBuilder.java
+++ src/main/java/com/ikiningyou/drserver/util/builder/user/UserBuilder.java
@@ -3,16 +3,15 @@ package com.ikiningyou.drserver.util.builder.user;
 import com.ikiningyou.drserver.model.dao.User;
 import com.ikiningyou.drserver.model.dto.user.UserResponse;
 
- public class UserBuilder {
-    
-     public static UserResponse UserToUserResponse(User user){
-      UserResponse newUser = UserResponse
-        .builder()
-        .id(user.getId())
-        .name(user.getName())
-        .phone(user.getPhone())
-        .lastTagged(user.getLastTagged())
-        .build();
-        return newUser;
-    }
+public class UserBuilder {
+
+  public static UserResponse UserToUserResponse(User user) {
+    return UserResponse
+      .builder()
+      .id(user.getId())
+      .name(user.getName())
+      .phone(user.getPhone())
+      .lastTagged(user.getLastTagged())
+      .build();
+  }
 }
