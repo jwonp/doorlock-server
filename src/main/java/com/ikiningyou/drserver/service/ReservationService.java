e10e18ecfcada0b5ba73507c73eaa883cd0b3c88
Joowon <tkdel222@gmail.com>
2023-06-25 22:06:44 +0900

    update reserve

--- src/main/java/com/ikiningyou/drserver/service/ReservationService.java
+++ src/main/java/com/ikiningyou/drserver/service/ReservationService.java
@@ -1,8 +1,15 @@
 package com.ikiningyou.drserver.service;
 
+import com.ikiningyou.drserver.model.dao.Card;
 import com.ikiningyou.drserver.model.dao.Reservation;
+import com.ikiningyou.drserver.model.dao.Room;
+import com.ikiningyou.drserver.model.dao.User;
+import com.ikiningyou.drserver.repository.CardRepository;
 import com.ikiningyou.drserver.repository.ReservationRepository;
+import com.ikiningyou.drserver.repository.RoomRepository;
+import com.ikiningyou.drserver.repository.UserRepository;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
@@ -12,6 +19,15 @@ public class ReservationService {
 
   @Autowired
   private ReservationRepository reservationRepository;
+
+  @Autowired
+  private CardRepository cardRepository;
+
+  @Autowired
+  private UserRepository userRepository;
+
+  @Autowired
+  private RoomRepository roomRepository;
 
   public Reservation[] getAllReservations() {
     List<Reservation> reservationList = reservationRepository.findAll();
@@ -25,14 +41,22 @@ public class ReservationService {
   }
 
   public Reservation addReservation(String userId, int roomId, String cardId) {
+    Optional<Card> card = cardRepository.findById(cardId);
+    Optional<Room> room = roomRepository.findById(roomId);
+    Optional<User> user = userRepository.findById(userId);
+    if (!card.isPresent() || !room.isPresent() || !user.isPresent()) {
+      return null;
+    }
     Reservation newReservation = Reservation
       .builder()
-      .userId(userId)
-      .cardId(cardId)
-      .roomId(roomId)
+      .card(card.get())
+      .user(user.get())
+      .room(room.get())
       .build();
     try {
-      Reservation savedReservation = reservationRepository.save(newReservation);
+      Reservation savedReservation = reservationRepository.saveAndFlush(
+        newReservation
+      );
       return savedReservation;
     } catch (IllegalArgumentException e) {
       e.printStackTrace();
