521b543f2878e820a557f17c112b8e8eb0cb41a0
Joowon <tkdel222@gmail.com>
2023-08-05 10:04:38 +0900

    add Strings

---
+++ src/main/java/com/ikiningyou/drserver/util/Strings.java
@@ -0,0 +1,8 @@
+package com.ikiningyou.drserver.util;
+
+public class Strings {
+
+  public static final String CARD_ADMIN = "ADMIN";
+  public static final String CARD_AUTHORIZED = "AUTHORIZED";
+  public static final String CARD_UNAUTHORIZED = "UNAUTHORIZED";
+}
