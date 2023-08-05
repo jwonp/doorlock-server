c6d210f4a32e3ec635302606a9d4cfe851019e8c
Joowon <tkdel222@gmail.com>
2023-08-05 10:03:21 +0900

    add auth controller

---
+++ src/main/java/com/ikiningyou/drserver/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.ikiningyou.drserver.controller;
+
+import com.ikiningyou.drserver.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+  @Autowired
+  private CardService cardService;
+
+  @GetMapping("/card")
+  public ResponseEntity<String> authorizeTaggedCard(
+    @RequestParam("id") String cardId
+  ) {
+    String result = cardService.authorizeCard(cardId);
+    HttpStatus status = HttpStatus.OK;
+    return ResponseEntity.status(status).body(result);
+  }
+}
