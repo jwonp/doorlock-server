71525424c4ba7be873601a20308dfa1f1df6f494
Joowon <tkdel222@gmail.com>
2023-08-05 11:00:49 +0900

    remove unused code

--- src/main/java/com/ikiningyou/drserver/model/dao/User.java
+++ src/main/java/com/ikiningyou/drserver/model/dao/User.java
@@ -4,7 +4,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.util.Date;
 import java.util.List;
