1f770f41df694ac791e0cdb23cc2d3a70fe1c8b8
Joowon <tkdel222@gmail.com>
2023-08-05 10:05:59 +0900

    remove prePersist

--- src/main/java/com/ikiningyou/drserver/model/dao/UserDetail.java
+++ src/main/java/com/ikiningyou/drserver/model/dao/UserDetail.java
@@ -3,7 +3,6 @@ package com.ikiningyou.drserver.model.dao;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
