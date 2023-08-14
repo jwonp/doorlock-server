aedaffe9be4782d188f5eb9fa2338d1075206ca0
Joowon <tkdel222@gmail.com>
2023-08-14 15:20:50 +0900

    update get to post on /auth/card

--- src/main/java/com/ikiningyou/drserver/controller/AuthController.java
+++ src/main/java/com/ikiningyou/drserver/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,7 +23,7 @@ public class AuthController {
   @Autowired
   private AuthService authService;
 
-  @GetMapping("/card")
+  @PostMapping("/card")
   public ResponseEntity<AuthAuthorizeCardResponse> authorizeTaggedCard(
     @RequestBody AuthAuthorizeCardRequest request
   ) {
