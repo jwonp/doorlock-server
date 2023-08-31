245b511cebd6d33008089ee972ec457bfc57dc40
Joowon <tkdel222@gmail.com>
2023-08-31 23:09:40 +0900

    update requsetMatchers

--- src/main/java/com/ikiningyou/drserver/config/WebConfig.java
+++ src/main/java/com/ikiningyou/drserver/config/WebConfig.java
@@ -1,6 +1,10 @@
 package com.ikiningyou.drserver.config;
 
+import com.ikiningyou.drserver.service.AuthService;
+import com.ikiningyou.drserver.util.Authorities;
 import com.ikiningyou.drserver.util.property.Endpoints;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,13 +15,19 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 public class WebConfig {
 
   @Autowired
   Endpoints endpoints;
+
+  @Autowired
+  private AuthService authService;
 
   @Value("${jwt.token.secret}")
   private String secretKey;
@@ -30,20 +40,28 @@ public class WebConfig {
         c.disable();
       })
       .csrf(c -> c.disable())
-      .cors(c -> c.disable()) // .cors(c -> {
-      //   CorsConfigurationSource source = request -> {
-      //     CorsConfiguration config = new CorsConfiguration();
+      // .cors(c -> c.disable())
+      .cors(c -> {
+        CorsConfigurationSource source = request -> {
+          CorsConfiguration config = new CorsConfiguration();
 
-      //     // log.info("endpoint is {}", endpoints.getFrontend());
-      //     config.setAllowedOrigins(List.of(endpoints.getFrontend()));
-      //     config.setAllowedMethods(
-      //       List.of("GET", "POST", "PUT", "PATCH", "DELETE")
-      //     );
-      //     config.setAllowedHeaders(List.of("X-IDENTIFIER"));
-      //     return config;
-      //   };
-      //   c.configurationSource(source);
-      // })
+          log.info(
+            "endpoint is {} Authorization is {}",
+            endpoints.getFrontend(),
+            request.getHeader("Authorization")
+          );
+          config.setAllowCredentials(true);
+          config.setAllowedOrigins(
+            List.of("http://127.0.0.1:3000", "http://localhost:3000")
+          );
+          config.setAllowedMethods(
+            List.of("GET", "POST", "PUT", "PATCH", "DELETE")
+          );
+          config.setAllowedHeaders(List.of("Authorization"));
+          return config;
+        };
+        c.configurationSource(source);
+      })
       .sessionManagement(c ->
         c.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
       )
@@ -53,15 +71,55 @@ public class WebConfig {
             HttpMethod.POST,
             "/auth/login",
             "/auth/logout",
-            "/auth/logout/success",
-            "/user"
+            "/user",
+            "/card/lost",
+            "/auth/card"
           )
           .permitAll()
+          .requestMatchers(
+            "/card/index",
+            "/room/select",
+            "/reservation/request"
+          )
+          .hasAuthority(Authorities.USER)
+          .requestMatchers(HttpMethod.GET, "/reservation")
+          .hasAuthority(Authorities.USER)
+          .requestMatchers(
+            "/auth/admin",
+            "/card",
+            "/card/admin/lost",
+            "/card/admin",
+            "/card/list",
+            "/card/list/reservation",
+            "/card/search",
+            "/card/lost/list",
+            "/user/admin",
+            "/user/list",
+            "/user/list/reservation",
+            "/user/search",
+            "/room",
+            "/room/admin",
+            "/room/list",
+            "/room/list/reservation",
+            "/room/search",
+            "/reservation/admin/reserved",
+            "/reservation/admin",
+            "/reservation/list",
+            "/reservation/list/user",
+            "/reservation/list/full",
+            "/reservation/full",
+            "/log/admin/unauthorized",
+            "/log/admin",
+            "/log/list"
+          )
+          .hasAuthority(Authorities.ADMIN)
+          .requestMatchers(HttpMethod.POST, "/reservation")
+          .hasAuthority(Authorities.ADMIN)
           .anyRequest()
           .authenticated();
       })
       .addFilterBefore(
-        new JwtFilter(secretKey),
+        new JwtFilter(authService, secretKey),
         UsernamePasswordAuthenticationFilter.class
       )
       .build();
