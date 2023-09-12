0d3250fad466246ec574f9fe4dfd3d36a189b5fc
Joowon <tkdel222@gmail.com>
2023-09-12 22:59:41 +0900

    update delete query

--- src/main/java/com/ikiningyou/drserver/repository/ReservedRequestRepository.java
+++ src/main/java/com/ikiningyou/drserver/repository/ReservedRequestRepository.java
@@ -3,10 +3,18 @@ package com.ikiningyou.drserver.repository;
 import com.ikiningyou.drserver.model.dao.ReservedRequest;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ReservedRequestRepository
   extends JpaRepository<ReservedRequest, Long> {
   @Query(value = "SELECT A FROM ReservedRequest A WHERE A.isAccepted = false")
   List<ReservedRequest> getAcceptedRequests();
+
+  @Transactional
+  @Modifying
+  @Query("delete from ReservedRequest where id = :id")
+  void deleteAllByIdInQuery(@Param("id") Long id);
 }
