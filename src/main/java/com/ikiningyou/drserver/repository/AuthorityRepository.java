6c8c2620f74e752e8157f75ab4b017319a87a958
Joowon <tkdel222@gmail.com>
2023-09-12 23:00:06 +0900

    update find by user to user detail

--- src/main/java/com/ikiningyou/drserver/repository/AuthorityRepository.java
+++ src/main/java/com/ikiningyou/drserver/repository/AuthorityRepository.java
@@ -1,9 +1,12 @@
 package com.ikiningyou.drserver.repository;
 
 import com.ikiningyou.drserver.model.dao.Authority;
+
+import com.ikiningyou.drserver.model.dao.UserDetail;
+
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthorityRepository extends JpaRepository<Authority, String> {
-  Optional<Authority> findByUser(String user);
+  Optional<Authority> findByUserDetail(UserDetail userDetail);
 }
