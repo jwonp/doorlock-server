a3cd06fa4120370b8c236bad77e5a7ab307e775d
Joowon <tkdel222@gmail.com>
2023-09-12 22:59:29 +0900

    remove unused code

--- src/main/java/com/ikiningyou/drserver/repository/UserDetailRepository.java
+++ src/main/java/com/ikiningyou/drserver/repository/UserDetailRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserDetailRepository
   extends JpaRepository<UserDetail, String> {
-  Optional<UserDetail> findUserDetailByUsername(String username);
+
 }
