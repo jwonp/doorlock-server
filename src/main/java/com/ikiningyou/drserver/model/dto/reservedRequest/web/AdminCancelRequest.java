0b483ac2d5ea6d8cc27da6ed3ffe03a539cf9b13
Joowon <tkdel222@gmail.com>
2023-09-12 20:47:09 +0900

    add cancel reserved request dto

---
+++ src/main/java/com/ikiningyou/drserver/model/dto/reservedRequest/web/AdminCancelRequest.java
@@ -0,0 +1,12 @@
+package com.ikiningyou.drserver.model.dto.reservedRequest.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AdminCancelRequest {
+    private Long requestId;
+}
