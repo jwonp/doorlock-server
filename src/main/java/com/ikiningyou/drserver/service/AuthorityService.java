8a714e7eb5e31518865c31b780d00f905e2c54ee
Joowon <tkdel222@gmail.com>
2023-09-12 23:00:31 +0900

    update user to user detail

--- src/main/java/com/ikiningyou/drserver/service/AuthorityService.java
+++ src/main/java/com/ikiningyou/drserver/service/AuthorityService.java
@@ -1,9 +1,14 @@
 package com.ikiningyou.drserver.service;
 
 import com.ikiningyou.drserver.model.dao.Authority;
+import com.ikiningyou.drserver.model.dao.User;
+import com.ikiningyou.drserver.model.dao.UserDetail;
 import com.ikiningyou.drserver.repository.AuthorityRepository;
+import com.ikiningyou.drserver.repository.UserDetailRepository;
+import com.ikiningyou.drserver.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,10 +17,16 @@ public class AuthorityService {
   @Autowired
   private AuthorityRepository authorityRepository;
 
-  public Authority addAuthority(String username, String authority) {
+  @Autowired
+  private UserDetailRepository userDetailRepository;
+
+  public Authority addAuthority(String userId, String authority) {
+    UserDetail userDetail = userDetailRepository
+      .findById(userId)
+      .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
     Authority saveAuthority = Authority
       .builder()
-      .user(username)
+      .userDetail(userDetail)
       .authority(authority)
       .build();
     try {
